7d777b35a7f8ec254bac5a1f32cd8a56c805039f
Rinju Sherpa <rinju54@gmail.com>
2020-11-29 22:19:36 -0600

    JavaSript objects

--- JavaScript/Hello.js
+++
@@ -1,9 +0,0 @@
-const names = { Rinju: "rinju", Sherpa: "sherpa", hello: "hello" };
-
-// My name is rinju
-// * imprtant part
-// ! Never touch it
-// ? I dont understand concept of closure, research this.
-// TODO: Write function for form.
-
-// ...
---
+++ JavaScript/objects.js
@@ -0,0 +1,22 @@
+//A Simple JavaScript Object..
+const car = {
+  color: "black",
+  brand: {
+    name: "Ford", //nested object
+    model: "Focus",
+  },
+  year: "2020",
+  description: function () {
+    console.log(
+      `This is ${this.brand.name} ${this.brand.model} made in ${this.year}`
+    );
+  },
+};
+
+car.description();
+
+car.color = "Blue"; //update the color
+car.brand.model = "Fiesta";
+delete car.year;
+
+//Output: This is ford focus made in 2020

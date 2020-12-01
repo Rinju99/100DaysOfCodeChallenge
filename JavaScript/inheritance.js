5edcf140d29457254b972037c0c98cc672fc2d76
Rinju Sherpa <rinju54@gmail.com>
2020-11-30 21:44:48 -0600

    simple class, constructor and inheritance example

---
+++ JavaScript/inheritance.js
@@ -0,0 +1,19 @@
+// Simple class and inheritance in JS
+class Day3A {
+  constructor(day) {
+    this.day = day;
+  }
+  days() {
+    return "This is " + this.day + " part-A";
+  }
+}
+
+class Day3B extends Day3A {
+  days() {
+    return super.days() + " and part-B combined";
+  }
+}
+
+const d = new Day3B("day3");
+
+//Output: This is day3 part-1 and part-2 combined..

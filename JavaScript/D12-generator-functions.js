d8d7152936f05e61a8c8802f19844f27a7770575
Rinju Sherpa <rinju54@gmail.com>
2020-12-09 23:23:44 -0600

    genetrator in action

--- JavaScript/D12-generator-functions.js
+++ JavaScript/D12-generator-functions.js
@@ -1,4 +1,4 @@
-/* A generator function an iterator rather than
+/* A generator function returns an iterator rather than
  giving a single return value like a normal functino does */
 
 function* powerOf(n) {
@@ -14,3 +14,11 @@ console.log(p.next().value); //5
 console.log(p.next().value); //25
 console.log(p.next().value); //125
 //similarly consecutives values will be returned
+
+//or you can call it like below..
+for (let p of powerOf(5)) {
+  if (p > 500) break;
+  console.log(p);
+}
+
+//Output: 5,15,125

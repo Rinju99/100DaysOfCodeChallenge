ba2ec0d1d7b6b14e31f700de3d929d2805acccb9
Rinju Sherpa <rinju54@gmail.com>
2021-01-04 23:06:33 -0600

    day 32

---
+++ JavaScript/D32.js
@@ -0,0 +1,6 @@
+const { createServer } = require("http");
+createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  request.on("data", (chunk) => response.write(chunk.toString().toUpperCase()));
+  request.on("end", () => response.end());
+}).listen(8000);

ea2a93f060ade300b76e35ce8164c9087baae2b7
Rinju Sherpa <rinju54@gmail.com>
2021-01-23 00:21:53 -0600

    day 44

--- JavaScript/D44-Express-API/index.js
+++ JavaScript/D44-Express-API/index.js
@@ -1,10 +1,21 @@
 import Express from "express";
+import item from "./items.js";
 const app = Express();
+const PORT = 8080;
 
-app.get("/", (req, res) => {
-  res.send("hello World");
+app.get("/item/id:", (req, res) => {
+  res.json(
+    item.find((items) => {
+      return +req.params.id === items.id;
+    })
+  );
 });
 
-app.listen(3000);
+app.post("/add", (req, res) => {
+  console.log(req.body.id);
+  res.sendStatus(200);
+});
+app.listen(PORT, () => console.log("listening at port 8080"));
 
-//app.post();
+//app.delete();
+//app.put();
---
+++ JavaScript/D44-Express-API/items.js
@@ -0,0 +1,20 @@
+const items = [
+  {
+    id: 1,
+    name: "apple",
+  },
+  {
+    id: 2,
+    name: "orange",
+  },
+  {
+    id: 3,
+    name: "banana",
+  },
+  {
+    id: 4,
+    name: "mango",
+  },
+];
+
+export default items;

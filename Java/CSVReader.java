978e3fad4f80e419173a93a9a83c110865efa93b
Rinju Sherpa <rinju54@gmail.com>
2021-01-25 23:14:17 -0600

    File reader java

--- Java/CSVReader.java
+++ Java/CSVReader.java
@@ -1,6 +1,23 @@
-public class CSVReader{
-  public static void Main(String[] args) {
-    System.out.println("hello");
-  }
-}
+package Java;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class CSVReader {
+
+	public static void main(String[] args) throws IOException,FileNotFoundException {
+		String filePath = "/Users/rinjusherpa/Documents/email.csv";
+		String line ="";
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		
+		while((line = reader.readLine()) !=null) {
+			String[] values = line.split(",");
+			System.out.print("Email: "+values[0]+" id: "+values[1]+ " Name: "+ values[3]+"\n");
+			
+    }
+    reader.close();
+
+	}
+
+}

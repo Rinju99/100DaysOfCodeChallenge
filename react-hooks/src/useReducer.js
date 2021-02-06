568c4e49dfd30a45cd650109f3e19eaa5750307b
Rinju Sherpa <rinju54@gmail.com>
2021-02-06 01:16:40 -0600

    useReducer-hook

---
+++ react-hooks/src/useReducer.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { rabbit: state.rabbit + 1 };
+    case "REMOVE":
+      return { rabbit: state.rabbit - 1 };
+    case "RESET":
+      return { dragon: 1 };
+    default:
+      return state;
+  }
+};
+
+export default function Reducer() {
+  const [state, dispatch] = useReducer(reducer, {
+    rabbit: 1,
+  });
+  return (
+    <>
+      <h1>{"🐇".repeat(state.rabbit)}</h1>
+      <button onClick={() => dispatch({ type: "ADD" })}>ADD</button>
+      <button onClick={() => dispatch({ type: "REMOVE" })}>REMOVE</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>RESET</button>
+    </>
+  );
+}

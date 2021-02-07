cbcde376f6fd940dc1496024ca095c60a98eeeef
Rinju Sherpa <rinju54@gmail.com>
2021-02-07 14:53:51 -0600

    monor updates

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./useReducer";
+import App from "./useRef";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(

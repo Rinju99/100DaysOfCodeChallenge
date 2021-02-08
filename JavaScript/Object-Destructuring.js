82f906b90b19ed613920b61deab8e737f6851ff3
Rinju Sherpa <rinju54@gmail.com>
2021-02-07 22:09:38 -0600

    object destructuring

--- JavaScript/Object-Destructuring.js
+++ JavaScript/Object-Destructuring.js
@@ -1,12 +1,13 @@
 // object destructuring in JavaScript..
-
 const coding = {
   day: "55",
-  topic: "destructuring",
+  topic: "object-destructuring",
 };
 
 function PrintOut({ day, topic }) {
-  console.log(`THis is my day ${day} and my topic for the day is ${topic}`);
+  console.log(`It's day ${day} and my topic for the day is ${topic}`);
 }
 
+// extracting 'day' and 'topic' from 'coding' and calling the function
 PrintOut(coding);
+// Output: It's my day 55 and my topic for the day is destructuring

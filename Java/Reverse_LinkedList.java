73da799d936bfeed389322ed17dd4435f57d074a
Rinju Sherpa <rinju54@gmail.com>
2021-02-10 14:07:33 -0600

    minor changes

--- Java/Reverse_LinkedList.java
+++ Java/Reverse_LinkedList.java
@@ -1,12 +1,12 @@
-public class Reverse_LinkedList {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        while(head!=null){
-            ListNode nxt = head.next;
-            head.next = prev_node;
-            prev = head;
-            head = nxt;
-        }
-        return prev; 
-    }
+class Solution {
+  public ListNode reverseList(ListNode curr_node) {
+      ListNode prev_node = null;
+      while(curr_node!=null){
+          ListNode temp = curr_node.next;
+          curr_node.next = prev_node;
+          prev_node = curr_node;
+          curr_node = temp;
+      }
+      return prev_node; 
+  }
 }

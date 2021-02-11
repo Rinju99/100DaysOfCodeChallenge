24071aaecdf04661dd7ee2e92ec0c74ffd0a3d6b
Rinju Sherpa <rinju54@gmail.com>
2021-02-11 08:11:05 -0600

    minor updates

--- JavaScript/Live-Mood-Detection/application.js
+++ JavaScript/Live-Mood-Detection/application.js
@@ -35,7 +35,6 @@ video.addEventListener("play", () => {
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
   }, 50);
 });

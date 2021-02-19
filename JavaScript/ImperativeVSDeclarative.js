4145b410750268623b8a90e72601d030f1ef3932
Rinju Sherpa <rinju54@gmail.com>
2021-02-19 00:43:02 -0600

    shuffle an array

--- JavaScript/ImperativeVSDeclarative.js
+++ JavaScript/ImperativeVSDeclarative.js
@@ -1,12 +1,12 @@
-//Imperative Programming...
+//Imperative Programming:How to achieve Sth
 const num = [1, 2, 3, 4, 5];
 const numDoubled = [];
 for (let i = 0; i < num.length; i++) {
   numDoubled[i] = num[i] * 2;
 }
-console.log(numDoubled);
+console.log(numDoubled); //[ 2, 4, 6, 8, 10 ]
 
-//Declarative Programming...
+//Declarative Programming: What should be achieved
 const number = [1, 2, 3, 4, 5];
 const numDouble = number.map((n) => n * 2);
-console.log(numDouble);
+console.log(numDouble); //[ 2, 4, 6, 8, 10 ]
---
+++ JavaScript/Shuffle.js
@@ -0,0 +1,11 @@
+// Shuffling a given array elements randomly
+const shuffle = (arr) => {
+  let l = arr.length;
+  while (l) {
+    const i = Math.floor(Math.random() * l--);
+    [arr[l], arr[i]] = [arr[i], arr[l]];
+  }
+  return arr;
+};
+const foo = [1, 2, 5, 9, 10, 15, 20];
+console.log(shuffle(foo));

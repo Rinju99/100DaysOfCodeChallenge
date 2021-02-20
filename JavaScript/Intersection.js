3bfc053e6f7028030bb50cd21e05341d3f3d296b
Rinju Sherpa <rinju54@gmail.com>
2021-02-20 00:20:37 -0600

    intersection between two array

--- JavaScript/Intersection.js
+++ JavaScript/Intersection.js
@@ -1,7 +1,8 @@
 // Intersection(common elements) between two arrays...
-const intersection = (a, b) => {
-  const _setB = new Set(b);
-  return a.filter((x) => _setB.has(x));
+
+const intersection = (arr1, arr2) => {
+  const _set = new Set(arr2);
+  return arr1.filter((x) => _set.has(x));
 };
 
 console.log(intersection([1, 2, 3, 7, 9], [1, 3, 5, 9]));

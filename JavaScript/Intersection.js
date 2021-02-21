a80972070baa97c5366f2944a6a71d929cd68990
Rinju Sherpa <rinju54@gmail.com>
2021-02-21 01:08:21 -0600

    Anagram in JS

--- JavaScript/Intersection.js
+++ JavaScript/Intersection.js
@@ -1,9 +1,6 @@
 // Intersection(common elements) between two arrays...
 
-const intersection = (arr1, arr2) => {
-  const _set = new Set(arr2);
-  return arr1.filter((x) => _set.has(x));
-};
+const intersection = (arr1, arr2) => arr1.filter((x) => arr2.includes(x));
 
 console.log(intersection([1, 2, 3, 7, 9], [1, 3, 5, 9]));
 // [1,3,9]

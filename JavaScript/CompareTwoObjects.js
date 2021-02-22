32b3b0fb9cf4ce6a827e12dbae4fdf97ef53ee84
Rinju Sherpa <rinju54@gmail.com>
2021-02-22 00:42:14 -0600

    comparing two objects in js

---
+++ JavaScript/CompareTwoObjects.js
@@ -0,0 +1,11 @@
+//Comparing two JSON-style object in JS...
+var obj1 = {
+  day: "68",
+  topic: "obj comparision",
+};
+var obj2 = {
+  day: "68",
+  topic: "obj comparision",
+};
+
+JSON.stringify(user1) === JSON.stringify(user2);

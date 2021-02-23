7757a340e0aeaf5ede8b79f7eec96dca62d8734c
Rinju Sherpa <rinju54@gmail.com>
2021-02-22 23:59:41 -0600

    minor update

---
+++ Java/Practice.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class Practice {
+  public static void main(String[] args) {
+
+    System.out.println("hello");
+  }
+  
+}
--- JavaScript/Digitize.js
+++ JavaScript/Digitize.js
@@ -1,5 +1,6 @@
 // Convert a number into an array of digits...
 
 const numberToArray = (n) => [...`${n}`].map((x) => parseInt(x));
+
 numberToArray(123);
 //Output: [ 1, 2, 3 ]

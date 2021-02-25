3ca3bbfa940de151c5094e5ae44aa10e6c7144d9
Rinju Sherpa <rinju54@gmail.com>
2021-02-24 23:40:25 -0600

    Counting repeatition of an element in an array

--- JavaScript/CountRepeat.js
+++ JavaScript/CountRepeat.js
@@ -1,4 +1,5 @@
-const countOccurrences = (arr, val) =>
-  arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+// Count how many times an element is repeated..
+const countRepeatition = (arr, val) =>
+  arr.reduce((i, v) => (v === val ? i + 1 : i), 0);
 
-console.log(countOccurrences([1, 1, 2, 1, 2, 3, 3, 4, 5], 1));
+console.log(countRepeatition([1, 1, 2, 1, 2, 3, 3, 4, 5, 1], 1));

a6c049517684feac05f82ef7c44a46a868aba5a8
Rinju Sherpa <rinju54@gmail.com>
2021-02-25 23:12:52 -0600

    filter out non-unique values

--- JavaScript/FilterUniqueValues.js
+++ JavaScript/FilterUniqueValues.js
@@ -1,4 +1,12 @@
-const filterNonUnique = (arr) =>
+// Filter out non-unique values...
+// const uniqueValue = (arr) =>
+//   arr.filter((i) => arr.indexOf(i) === arr.lastIndexOf(i));
+
+// console.log(uniqueValue([1, 2, 3, 3, 4, 4, 5]));
+// Ouput: [ 1, 2, 5 ]
+
+const unique = (arr) =>
   arr.filter((i) => arr.indexOf(i) === arr.lastIndexOf(i));
 
-console.log(filterNonUnique([1, 2, 2, 3, 4, 4, 5]));
+console.log(unique([1, 2, 3, 3, 4, 4, 5]));
+//Ouput: [1, 2, 5];

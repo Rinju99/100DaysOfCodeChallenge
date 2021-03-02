9513999f01c47ef3dec60368efb83d7801406c30
Rinju Sherpa <rinju54@gmail.com>
2021-03-01 23:21:14 -0600

    Common Element from 2 arrays

--- JavaScript/CommonElementArr.js
+++ JavaScript/CommonElementArr.js
@@ -1,3 +1,4 @@
-const matching = (arr, val) => arr.filter((v) => val.includes(v));
+// Find matching value from 2 arays..
+const matching = (arr1, arr2) => arr1.filter((v) => arr2.includes(v));
 match = matching([1, 2, 3, 5, 7], [1, 2, 4, 7]);
-console.log(match); /// [ 1, 2, 7 ]
+console.log(match); // [ 1, 2, 7 ]

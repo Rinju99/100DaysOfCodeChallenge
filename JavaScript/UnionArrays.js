dd442bab48f61d18d8ec06bf07b76ae511a9db8c
Rinju Sherpa <rinju54@gmail.com>
2021-03-04 23:39:37 -0600

    Union of arrays

--- JavaScript/UnionArrays.js
+++ JavaScript/UnionArrays.js
@@ -3,5 +3,5 @@ const arrUnion = (a, b, func) => {
   const s = new Set(a.map(func));
   return new Set([...a, ...b.filter((x) => !s.has(func(x)))]);
 };
-console.log(arrUnion([1, 5, 7], [1, 7, 13], Math.floor));
+console.log(arrUnion([1, 5, 7], [1, 7, 13, 17], Math.floor));
 // [ 1, 5, 7, 13 ]

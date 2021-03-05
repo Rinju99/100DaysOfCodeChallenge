d3d678e67c6543d766c3b3bdf5fe41e1e704cd6b
Rinju Sherpa <rinju54@gmail.com>
2021-03-05 00:12:43 -0600

    Sort String in JS

--- JavaScript/SortingString.js
+++ JavaScript/SortingString.js
@@ -1,5 +1,6 @@
+// Sorting string in alphabetical order...
 const sortString = (str) =>
   [...str].sort((a, b) => a.localeCompare(b)).join("");
 
-const s = sortString("axbre"); // 'aabbceg'
-console.log(s);
+const s = sortString("eaiou");
+console.log(s); // aeiou

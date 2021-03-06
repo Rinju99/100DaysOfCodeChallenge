b047d19e5c6a91dcf127fbbf37b7aa64ad708b98
Rinju Sherpa <rinju54@gmail.com>
2021-03-05 23:51:19 -0600

    Spread over operator

--- JavaScript/SpreadOver.js
+++ JavaScript/SpreadOver.js
@@ -1,3 +1,7 @@
-const spreadOver = (fn) => (argsArr) => fn(...argsArr);
-const arrayMax = spreadOver(Math.min);
+const spreadOver = (fn) => (args) => fn(...args);
+
+const arrayMax = spreadOver(Math.max);
 console.log(arrayMax([1, 2, 3])); // 3
+
+const arrayMin = spreadOver(Math.min);
+console.log(arrayMin([1, 2, 3])); // 1

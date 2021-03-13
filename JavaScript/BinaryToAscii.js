c7940fd880e181711a074750d0a195ffb15a3829
Rinju Sherpa <rinju54@gmail.com>
2021-03-13 00:55:02 -0600

    encode string in base64 ascii

--- JavaScript/BinaryToAscii.js
+++ JavaScript/BinaryToAscii.js
@@ -1,4 +1,4 @@
 // Convert string into base-64 incoded ASCII
 const encoded = (str) => Buffer.from(str, "binary").toString("base64");
 
-console.log(encoded("software")); // amF2YXNjcmlwdA==
+console.log(encoded("Sherpa")); //Y29kaW5n

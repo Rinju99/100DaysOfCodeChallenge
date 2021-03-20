36d548e95bb1941095269ed26ca74632d9bc0665
Rinju Sherpa <rinju54@gmail.com>
2021-03-20 01:46:16 -0500

    armstrong num

---
+++ JavaScript/ArmstrongNum.js
@@ -0,0 +1,12 @@
+// A function to test armstrong number..
+function armstrongNum(num) {
+  const str = num + "";
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum += Math.pow(Number(str[i]), str.length);
+  }
+  return sum === num;
+}
+
+console.log(armstrongNum(153));
+console.log(armstrongNum(1634));

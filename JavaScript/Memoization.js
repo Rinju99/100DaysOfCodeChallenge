4d9ee5a3e41e64ffe0e334da3c8d1e6a7f3594a0
Rinju Sherpa <rinju54@gmail.com>
2021-03-21 23:47:25 -0500

    memoization

--- JavaScript/Memoization.js
+++ JavaScript/Memoization.js
@@ -14,5 +14,5 @@ const memoized = () => {
   };
 };
 const memo = memoized();
-console.log(memo(5)); //Calculating
-console.log(memo(5)); //taking from cache
+console.log(memo(5)); //Calculating the result 15
+console.log(memo(5)); //taking from cache 15

9e4f0e02782fed9059b2af9eef314c8245ac1ef8
Rinju Sherpa <rinju54@gmail.com>
2021-03-21 23:41:06 -0500

    memoization

---
+++ JavaScript/Memoization.js
@@ -0,0 +1,18 @@
+// Memoization technique in action...
+const memoized = () => {
+  let cache = {};
+  return (i) => {
+    if (i in cache) {
+      console.log("taking from cache");
+      return cache[i];
+    } else {
+      console.log("Calculating the result");
+      let output = i + 10;
+      cache[i] = output;
+      return output;
+    }
+  };
+};
+const memo = memoized();
+console.log(memo(5)); //Calculating
+console.log(memo(5)); //taking from cache
